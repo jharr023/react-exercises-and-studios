2659b769de7d7c356d62d80204bcdb02e0060d02
jharr023 <jharringtonkc@gmail.com>
2024-03-11 18:04:47 -0500

    commit

--- part1/exercises/src/App.jsx
+++ part1/exercises/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import MovieList from './components/MovieList.jsx';
 import ChoresList from './components/ChoresList.jsx';
 import BookList from './components/BookList.jsx';
+import HobbyLinks from './components/HobbyLinks.js';
 
 class App extends Component  {
 
